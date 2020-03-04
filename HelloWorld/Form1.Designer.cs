4e2eb2a42307843a438fa008ce6f8d4b039b702e
liangshimao <879088237@qq.com>
2020-03-04 10:43:56 +0800

    first

---
+++ HelloWorld/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace HelloWorld
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("隶书", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(109, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(135, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始执行";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("隶书", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(318, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "关闭窗口";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Georgia", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(29, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(344, 31);
+            this.textBox1.TabIndex = 3;
+            //this.textBox1.Text = "D:\\personal";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("隶书", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(379, 99);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "选择路径";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("隶书", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(76, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "提示：";
+            
+            // 
+            // label3
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("隶书", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(139, 157);
+            this.label2.Name = "label3";
+            this.label2.Size = new System.Drawing.Size(29, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "无";
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 412);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("隶书", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Name = "Form1";
+            this.Text = "excel修改工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HelloWorld/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Office.Core;
+using MSword = Microsoft.Office.Interop.Excel;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(textBox1.Text.ToString());
+            string path = textBox1.Text.ToString();
+
+            DirectoryInfo TheFolder = new DirectoryInfo(path);
+            if (!TheFolder.Exists) {
+                MessageBox.Show("文件夹不存在！", "Warning");
+                return;
+            }
+
+            foreach (FileInfo NextFile in TheFolder.GetFiles())
+            {
+                if (Path.GetExtension(NextFile.Name) != ".xls" && Path.GetExtension(NextFile.Name) != ".xlsx")
+                {
+                    continue;
+                }
+
+                label2.Text = "正在处理" + NextFile.Name;
+                // 获取文件完整路径
+                string fileName = NextFile.FullName;
+                //string extension = Path.GetExtension(heatmappath);
+                //MessageBox.Show(heatmappath, "Warning");
+                MSword.Application EXC1 = new MSword.Application();
+                //EXC1.Visible = true;
+                MSword.Workbooks wbs = EXC1.Workbooks;
+                MSword.Workbook wb = wbs.Add(fileName);//打开并显示EXCEL文件
+                MSword.Worksheet Exsheet;
+                Exsheet = wb.Sheets[1];
+                //Exsheet.Cells[1,1] = "liang";
+                //Console.WriteLine(Exsheet.Name);
+                int sRowcount = Exsheet.UsedRange.Columns.Count;
+                Console.WriteLine(sRowcount);
+                Exsheet.Activate();
+
+                Exsheet.Cells[1, ++sRowcount] = "是否党员";
+                Exsheet.Cells[1, ++sRowcount] = "交通工具";
+                Exsheet.Cells[1, ++sRowcount] = "未住户情况";
+                //Exsheet.Cells[1, 1] = "liang111";
+
+                //object MisValue = Type.Missing;
+                //string[] insertName = new string[] { "是否党员", "交通工具", "未住户情况" };
+                /*
+                for (int i = 0;i < 3;i++) 
+                {
+                    MSword.Range xlsColumns = (MSword.Range)Exsheet.Columns[2, MisValue];
+                    xlsColumns.EntireColumn.Insert(MSword.XlDirection.xlToRight, MSword.XlInsertFormatOrigin.xlFormatFromLeftOrAbove);
+                }
+                */
+                //xlsColumns.Insert();
+
+
+                EXC1.DisplayAlerts = false;
+                wb.SaveAs(fileName, Type.Missing, "", "", Type.Missing, Type.Missing, MSword.XlSaveAsAccessMode.xlNoChange, 1, false, Type.Missing, Type.Missing, Type.Missing);
+
+
+
+                label2.Text = "已完成" + NextFile.Name;
+                wb.Close();//关闭文档
+                wbs.Close();//关闭工作簿
+
+                EXC1.Quit();//关闭EXCEL应用程序
+
+                //释放EXCEL应用程序的进程
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(EXC1);
+
+            }
+
+            label2.Text = "已完成全部";
+
+
+        }
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dilog = new FolderBrowserDialog();
+            dilog.Description = "请选择文件夹";
+            if (dilog.ShowDialog() == DialogResult.OK || dilog.ShowDialog() == DialogResult.Yes)
+            {
+                textBox1.Text = dilog.SelectedPath;
+            }
+        }
+
+        
+    }
+}
